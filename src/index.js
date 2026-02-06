0417667b9069f6936f7e874c6ef3f4394170124b
Anurag Mourya <anuragmourya901@gmail.com>
2026-02-06 20:54:40 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+
+dotenv.config({
+    path: './env'
+});
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+
+( async ()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+       app.on("error",(error)=>{
+        console.log("ERR",error);
+        throw error;
+        
+       })
+
+
+         app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+            
+         })
+
+        
+    } catch (error) {
+        console.error("Error:",error);
+        
+        
+    }
+}) (); 
+ */
+ 
